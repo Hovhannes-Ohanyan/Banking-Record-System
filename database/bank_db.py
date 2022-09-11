088d77eea12ddb569e3addbfb7bf8a74a3a00684
Hovhannes <hovhannes.ohanyan11@mail.ru>
2022-09-11 15:26:57 +0400

    Successfully finished

---
+++ database/bank_db.py
@@ -0,0 +1,154 @@
+"""This file create db and table and is connected with sql"""
+
+import sqlite3
+
+con = sqlite3.connect("database/bank_recording.db")
+cursor = con.cursor()
+cursor.execute('''
+          CREATE TABLE IF NOT EXISTS bank_users(
+                name text,
+                surname text,
+                card_number text,
+                money int
+          ) ''')
+
+
+def create_user(name, surname, card_number, money):
+    """This function is creates user"""
+
+    connection = sqlite3.connect('database/bank_recording.db')
+    cursor = connection.cursor()
+    with connection:
+        cursor.execute("INSERT INTO bank_users VALUES (:name, :surname, :card_number, :money)",
+                       {'name': name, 'surname': surname,
+                        'card_number': card_number, 'money': money})
+
+
+def get_all_users_card_number():
+    """This function return user's balance"""
+
+    con = sqlite3.connect('database/bank_recording.db')
+    cursor = con.cursor()
+    with con:
+        cursor.execute("SELECT * FROM bank_users")
+        data = cursor.fetchall()
+        lst = []
+        for cont in data:
+            lst.append(cont[2])
+        return lst
+
+
+def get_money_from_card_number(card_number):
+    """This function return user's balance"""
+
+    con = sqlite3.connect('database/bank_recording.db')
+    cursor = con.cursor()
+    with con:
+        cursor.execute("SELECT * FROM bank_users")
+        data = cursor.fetchall()
+        lst = []
+        for d in data:
+            lst.append(d)
+        for el in lst:
+            if el[2] == card_number:
+                return el[3]
+        return -1
+
+
+def get_surname_from_name(name):
+    """This function return list of user's surname"""
+    con = sqlite3.connect('database/bank_recording.db')
+    cursor = con.cursor()
+    with con:
+        cursor.execute("SELECT * FROM bank_users")
+        data = cursor.fetchall()
+        lst = []
+        lst_surnames = []
+        for d in data:
+            lst.append(d)
+        for el in lst:
+            if el[0] == name:
+                lst_surnames.append(el[1])
+        return lst_surnames
+
+
+def get_card_number_from_name_surname(name, surname=None):
+    """This function is return card number by name and surname"""
+    con = sqlite3.connect('database/bank_recording.db')
+    cursor = con.cursor()
+    with con:
+        cursor.execute("SELECT * FROM bank_users")
+        data = cursor.fetchall()
+        lst = []
+        for d in data:
+            lst.append(d)
+        for el in lst:
+            if el[0] == name and surname == None:
+                return el[2]
+            elif el[0] == name and el[1] == surname:
+                return el[2]
+
+
+def update_balance(card_number, money):
+    """ This function is updates money from card number"""
+    if card_number in get_all_users_card_number():
+        balance = get_money_from_card_number(card_number)
+        money = int(balance) + int(money)
+        con = sqlite3.connect('database/bank_recording.db')
+        cursor = con.cursor()
+        with con:
+            cursor.execute("UPDATE bank_users SET money=:money WHERE card_number=:card_number",
+                           {'money': str(money), 'card_number': card_number})
+
+    else:
+        print("Write valid card number and enter Update Balance option again")
+
+
+def transfer_balance(card_number_from, card_number_to, money):
+    """This function is transfer money from one account to another"""
+    if (card_number_from and card_number_to) in get_all_users_card_number():
+        if get_money_from_card_number(card_number_from) >= int(money):
+            update_balance(card_number_to, money)
+            update_balance(card_number_from, f'-{money}')
+            print("Done")
+        else:
+            print("There is not enough money ")
+
+    else:
+        print("Write valid card numbers and enter the Transfer Money option again")
+
+
+def show_all_users():
+    """This function is returns all users"""
+    con = sqlite3.connect('bank_recording.db')
+    cursor = con.cursor()
+    with con:
+        cursor.execute("SELECT * FROM bank_users")
+        users = cursor.fetchall()
+        lst = []
+        for u in users:
+            lst.append(u)
+        return lst
+
+
+def show_users_sort_by_balance():
+    """This function is returns all users,but sorted by balance"""
+    con = sqlite3.connect('bank_recording.db')
+    cursor = con.cursor()
+    with con:
+        cursor.execute("SELECT * FROM bank_users ORDER BY money")
+        users = cursor.fetchall()
+        lst = []
+        for u in users:
+            lst.append(u)
+        return lst
+
+
+def delete_user(card_number):
+    """This function delete user"""
+
+    connection = sqlite3.connect('bank_recording.db')
+    cur = connection.cursor()
+    with connection:
+        cur.execute("DELETE from bank_users WHERE card_number = :card_number",
+                    {'card_number': card_number})
